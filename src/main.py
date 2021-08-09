0056402b8f4d15223500a6f919c2afcdb5c30beb
lyubolp <lyubo.karev@gmail.com>
2021-08-09 15:38:28 +0300

    Added filters

--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
 import os
-from PIL import Image, ImageEnhance
+from enum import Enum
+from PIL import Image, ImageEnhance, ImageFilter
 
 from cli import parse_args
 from presets import read_presets
 
 
+class ActionTypes(Enum):
+    enhanceAction = 0,
+    filter = 1
+
+
 def generate_object_from_action(action_name):
     objects = {
-        "color": ImageEnhance.Color,
-        "contrast": ImageEnhance.Contrast,
-        "brightness": ImageEnhance.Brightness
+        "color": (ImageEnhance.Color, ActionTypes.enhanceAction),
+        "contrast": (ImageEnhance.Contrast, ActionTypes.enhanceAction),
+        "brightness": (ImageEnhance.Brightness, ActionTypes.enhanceAction),
+        "sharpness": (ImageEnhance.Sharpness, ActionTypes.enhanceAction),
+        "blur": (ImageFilter.BLUR, ActionTypes.filter),
+        "contour": (ImageFilter.CONTOUR, ActionTypes.filter),
+        "detail": (ImageFilter.DETAIL, ActionTypes.filter),
+        "edge_enhance": (ImageFilter.EDGE_ENHANCE, ActionTypes.filter),
+        "sharpen": (ImageFilter.SHARPEN, ActionTypes.filter),
+        "smooth": (ImageFilter.SMOOTH, ActionTypes.filter),
+
     }
 
     return objects[action_name]
@@ -22,8 +36,8 @@ if __name__ == "__main__":
     presets = read_presets()
 
     if args["verbosity"]:
-        print(args)
-        print(presets)
+        print("Args: {}".format(args))
+        print("Presets: {}".format(presets))
 
     source_image = args["source"]
 
@@ -50,7 +64,15 @@ if __name__ == "__main__":
 
         for step in steps:
             print("Applying {} with value {}".format(step[0], step[1]))
-            obj = generate_object_from_action(step[0])(im)
-            im = obj.enhance(step[1])
+
+            module, action_type = generate_object_from_action(step[0])
+
+            if action_type == ActionTypes.enhanceAction:
+                obj = module(im)
+                im = obj.enhance(step[1])
+            elif action_type == ActionTypes.filter:
+                im = im.filter(obj)
 
         im.save(target_image)
+
+    print("Done")
