6ef7ac4899acd071f12c11fe7f9dc81645ea8c77
lyubolp <lyubo.karev@gmail.com>
2021-08-09 15:38:17 +0300

    CLI changes

--- src/cli.py
+++ src/cli.py
@@ -3,17 +3,17 @@ import argparse
 
 def parse_args() -> dict:
     parser = argparse.ArgumentParser(
+        prog="photographerMacros",
         description="A CLI tool which applies filters to photos",
         add_help=True
     )
 
-    parser.add_argument("source", type=str)
-    parser.add_argument("target", type=str)
-    parser.add_argument("preset", type=str)
+    parser.add_argument("source", type=str, help="Source image")
+    parser.add_argument("target", type=str, help="Destination path")
+    parser.add_argument("preset", type=str, help="Which preset to be applied")
 
-    parser.add_argument("--color", type=float, default=1.0)
-    parser.add_argument("--contrast", type=float, default=1.0)
-    parser.add_argument("-v", "--verbosity", action="store_true", default=False)
+    parser.add_argument("-v", "--verbosity", action="store_true", default=False, help="Show more info on stdout")
+    # parser.add_argument("-q", "--quiet", action="store_true", default=False, help="Doesn't show anything on the stdout")
 
     args = parser.parse_args()
 
