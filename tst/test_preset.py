e88994c41ec020c1602d0e6b069b85fe5b2dfcf9
lyubolp <lyubo.karev@gmail.com>
2021-09-17 15:37:46 +0300

    Unittests

--- tst/test_preset.py
+++ tst/test_preset.py
@@ -1,8 +1,40 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from preset import ActionTypes, Step, Preset
+
+
+class TestStep(unittest.TestCase):
+    def test_01_constructor_and_properties(self):
+        temp_name = "name"
+        temp_value = "value"
+        temp_executable = lambda x: x
+        temp_action_type = ActionTypes.filter
+
+        temp_instance = Step(temp_name, temp_value, temp_executable, temp_action_type)
+
+        self.assertEqual(temp_name, temp_instance.name)
+        self.assertEqual(temp_value, temp_instance.value)
+        self.assertEqual(temp_executable, temp_instance.executable)
+        self.assertEqual(temp_action_type, temp_instance.action_type)
+
+
+class TestPreset(unittest.TestCase):
+    def test_01_constructor_and_properties(self):
+        temp_names = ["name1", "name2", "name3"]
+        temp_values = ["value1", "value2", "value3"]
+        temp_executables = [lambda x: x, lambda y: y + 2, lambda z: z + 3]
+        temp_action_types = [ActionTypes.filter, ActionTypes.enhanceAction, ActionTypes.custom]
+
+        steps = [Step(temp_names[i], temp_values[i], temp_executables[i], temp_action_types[i]) for i in range(len(temp_names))]
+
+        instance_name = "foo"
+        temp_instance = Preset(instance_name, steps)
+
+        self.assertEqual(instance_name, temp_instance.name)
+
+        for i, step in enumerate(temp_instance.steps):
+            self.assertEqual(steps[i], step)
+
 
 if __name__ == '__main__':
     unittest.main()
