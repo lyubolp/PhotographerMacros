5324f65ee559cece46cc22383a883dad5264e28d
lyubolp <lyubo.karev@gmail.com>
2023-06-04 19:07:47 +0300

    Modified the shell script to check if an argument is actually a file

--- photographerMacros.sh
+++ photographerMacros.sh
@@ -9,14 +9,14 @@ source .venv/bin/activate
 
 PYTHONPATH="$executable_dir"
 
-if [[ -n $1 ]] && [[ $1 != -* ]]
+if [[ -n $1 ]] && [[ $1 != -* ]] && stat "$1" >/dev/null 2>&1 
 then
   first="$current_dir"/"$1"
 else
   first="$1"
 fi
 
-if [[ -n $2 ]] && [[ $2 != -* ]]
+if [[ -n $2 ]] && [[ $2 != -* ]] && stat "$2" >/dev/null 2>&1 
 then
   second="$current_dir"/"$2"
 else
