cdae459caa28f60850e7f431328026a69592500c
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-08-17 23:05:54 +0300

    added a function that executes the given folder operation, fitlering the files if needed

--- src/execution.py
+++ src/execution.py
@@ -1,9 +1,11 @@
 """
-Modules containing all execution logic for Presets and Queries
+Modules containing all execution logic for Presets, Queries and Folder operations
 """
-from typing import List
+import os
+from typing import List, Optional
 from PIL import Image
 
+from src.folder_operations import BaseOperation, BaseCriteria
 from src.query import Query
 
 
@@ -20,3 +22,20 @@ def execute_query(query_name: str, queries: List[Query], source_image: str):
         query = [q for q in queries if q.name == query_name][0]
         value = query.executable(image)
         return value
+
+# TODO: Add error handling
+def execute_folder_operation(operation: BaseOperation, source_dir: str, destination_dir: str = '',
+                             criteria: Optional[BaseCriteria] = None):
+    """
+    Executes a folder operation on a directory
+    """
+    if criteria is not None:
+        files = criteria.filter(source_dir)
+    else:
+        files = os.listdir(source_dir)
+
+    for filepath in files:
+        source_path = os.path.join(source_dir, filepath)
+        destination_path = os.path.join(destination_dir, filepath)
+
+        operation.execute(source_path, destination_path)
